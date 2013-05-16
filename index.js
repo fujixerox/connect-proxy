4ce0b2de2afba8fbf113ff0c93e95a411e43fa66
Toshiya SAITOH <stoshiya@gmail.com>
2013-05-16 22:09:34 +0900

    Implemented error handler and corrected comment block.

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
-#------------------------------------------------------
-# 
-# noda shinpei 
-# 2015/5/16
-# relay server between local and github
-# local <--> git.rdh <--> enterprise proxy <--> github
-#
-#------------------------------------------------------
-
-process.on('uncaughtException', function(err) {
-    console.log(err.stack);
-});
+/*
+ * Copyright Fuji Xerox Co., Ltd. 2013 All rights reserved.
+ *
+ * relay server between local and github
+ * local <--> internal <--> proxy <--> github
+ */
 
 var net  = require('net');
-var server = net.createServer(function(socket) {
-    var buffer = '';
-    socket.on('data', function(data) {
-        buffer += data.toString();
-        if (buffer.indexOf('\r\n\r\n') > 0) {
-            var captures = buffer.match(/^CONNECT ([^:]+):([0-9]+) HTTP\/1\.[01]/);
 
-            if (!captures || captures.length < 2)
-                return;
-            socket.removeAllListeners('data');
-
-            var host = captures[1];
-            var port = captures[2] || 22;
-
-            var proxy = net.createConnection(8080, '129.249.115.194', function() {
-                proxy.write('CONNECT ' + host + ':' + port + " HTTP/1.0\r\n\r\n");
-            });
-            socket.pipe(proxy);
-            proxy.pipe(socket);
-        }
-    });
+process.on('uncaughtException', function(err) {
+  printError(err);
 });
-server.listen(8080);
+
+if (!process.env.PROXY_HOST || !process.env.PROXY_PORT) {
+  printError('require proxy host and port.');
+  process.exit(1);
+}
+
+net.createServer(function(socket) {
+  var buffer = '';
+  socket.on('connect', function() {
+    console.log('[' + Date.now().toUTCString() + '] ' + 'connect from ' + socket.remoteAddress);
+  });
+  socket.on('data', function(data) {
+    buffer += data.toString();
+    if (buffer.indexOf('\r\n\r\n') > 0) {
+      var captures = buffer.match(/^CONNECT ([^:]+):([0-9]+) HTTP\/1\.[01]/);
+
+      if (!captures || captures.length < 2)
+        return;
+      socket.removeAllListeners('data');
+
+      var host = captures[1];
+      var port = captures[2] || 22;
+
+      var proxy = net.createConnection(process.env.PROXY_PORT, process.env.PROXY_HOST, function() {
+        proxy.write('CONNECT ' + host + ':' + port + " HTTP/1.0\r\n\r\n");
+      });
+      socket.pipe(proxy);
+      proxy.pipe(socket);
+    }
+  });
+  socket.on('error', function(err) {
+    printError(err);
+  });
+}).listen(8080, function() { console.log('Starting...') });
+
+function printError(err) {
+  console.error('[' + new Date().toString() + '] ' + err);
+}
