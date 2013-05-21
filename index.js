2b71d7f1a303a48e5f7953073a241ddfa9100c3d
notti <noda.shinpei@fujierox.co.jp>
2013-05-21 09:52:06 +0900

    commit connect-proxy

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ net.createServer(function(socket) {
   console.log('[' + new Date().toString() + '] ' + 'connect from ' + socket.remoteAddress);
 
   var buffer = '';
-  socket.on('readable', function(data) {
+  socket.on('data', function(data) {
     buffer += data.toString();
     if (buffer.indexOf('\r\n\r\n') > 0) {
       var captures = buffer.match(/^CONNECT ([^:]+):([0-9]+) HTTP\/1\.[01]/);
 
       if (!captures || captures.length < 2)
         return;
-      socket.removeAllListeners('readable');
+      socket.removeAllListeners('data');
 
       var host = captures[1];
       var port = captures[2] || 22;
